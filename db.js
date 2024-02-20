a530b533160f8b52b0e53f7c84c2668fb7454457
Metin Cakmak <metin_cakmak@gmx.de>
2024-02-20 21:58:18 +0100

    Remove deprecated mongodb connect options

--- db.js
+++ db.js
@@ -3,20 +3,18 @@ import { config } from "dotenv";
 
 config();
 
-const mongoUri = process.env.MONGO_URI_SLS; 
+const mongoUri = process.env.MONGO_URI_SLS;
 let conn = null;
 
-async function connectDatabase () {
-    if(conn == null) {
-        console.log('Creating new connection to database...');
-        conn = connect(`${mongoUri}`, {
-          serverSelectionTimeoutMS: 5000,
-          useNewUrlParser: true,
-          useUnifiedTopology: true
-        });
-        return conn;
-    }
-    console.log('Connection already established, reusing the connection');
+async function connectDatabase() {
+  if (conn == null) {
+    console.log("Creating new connection to database...");
+    conn = connect(`${mongoUri}`, {
+      serverSelectionTimeoutMS: 5000,
+    });
+    return conn;
+  }
+  console.log("Connection already established, reusing the connection");
 }
 
 const _connectDatabase = connectDatabase;
